0198be5571ff13d922cd820d65d63189a34eeb23
Bubble_Interface <Mishareznik@yandex.ru>
2021-02-05 10:42:46 +0300

    POST method redirects to home page and user input displayed in the table

--- lists/tests.py
+++ lists/tests.py
@@ -17,9 +17,15 @@ class SmokeTest(TestCase):
         self.assertTemplateUsed(responce, 'home.html')
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_can_redirect_a_POST_request(self):
+        responce = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(responce.status_code, 302)
+        self.assertEqual(responce['location'], '/')
 
 class ItemModelTest(TestCase):
 
@@ -39,3 +45,18 @@ class ItemModelTest(TestCase):
         second_saved_item = all_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'The second item')
+
+class HomePageTest(TestCase):
+
+    def test_only_save_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_display_all_list_items(self):
+        Item.objects.create(text='Item_1')
+        Item.objects.create(text='Item_2')
+
+        responce = self.client.get('/')
+
+        self.assertIn('Item_1', responce.content.decode())
+        self.assertIn('Item_2', responce.content.decode())
