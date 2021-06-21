fbacc3b537c4d59fd6a75c233ec98e187188c687
Bubble_Interface <Mishareznik@yandex.ru>
2021-06-21 16:53:18 +0300

    added url for creating a new lists + tests

--- lists/tests.py
+++ lists/tests.py
@@ -13,19 +13,8 @@ class SmokeTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_the_correct_url(self):
-        responce = self.client.get('/')
-        self.assertTemplateUsed(responce, 'home.html')
-
-    def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_can_redirect_a_POST_request(self):
-        responce = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(responce.status_code, 302)
-        self.assertEqual(responce['location'], '/lists/the-only-list-in-the-world/')
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
 class ItemModelTest(TestCase):
 
@@ -45,24 +34,31 @@ class ItemModelTest(TestCase):
         second_saved_item = all_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'The second item')
-
-class HomePageTest(TestCase):
-
-    def test_only_save_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
+        
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
-        responce = self.client.get('/lists/the-only-list-in-the-world/')
-        self.assertTemplateUsed(responce, 'list.html')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
 
     def test_display_all_list_items(self):
         Item.objects.create(text='Item_1')
         Item.objects.create(text='Item_2')
 
-        responce = self.client.get('/lists/the-only-list-in-the-world/')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-        self.assertContains(responce, 'Item_1')
-        self.assertContains(responce, 'Item_2')
+        self.assertContains(response, 'Item_1')
+        self.assertContains(response, 'Item_2')
+
+
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+    
+    def test_can_redirect_a_POST_request(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
